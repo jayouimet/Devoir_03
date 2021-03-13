1cafd9636afc91dafdaa5fe020528a2e20bcee37
jayouimet <jayouimet@hotmail.com>
2021-03-13 16:13:01 -0500

    Auth et oubli mdp
    Pour l'authentification, le courriel est Test  et le mdp est 1234
    J'ai changé quelques couleurs du thème dans themes.xml
    La MainActivity existe encore mais dans AndroidManifest.xml j'ai changé l'activité de départ pour LoginActivity.
    À voir le TODO dans LoginActivity pour changer à la prochaine activitée post login.

---
+++ Devoir03/app/src/main/java/com/example/devoir03/ForgotPwdActivity.java
@@ -0,0 +1,31 @@
+package com.example.devoir03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ForgotPwdActivity extends AppCompatActivity {
+    TextView forgotPasswordMessageSentLabel;
+    Button forgotPasswordReinitialiseButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_pwd);
+
+        this.forgotPasswordReinitialiseButton = findViewById(R.id.forgotPasswordReinitialiseButton);
+        this.forgotPasswordMessageSentLabel = findViewById(R.id.forgotPasswordMessageSentLabel);
+        this.forgotPasswordMessageSentLabel.setVisibility(View.GONE);
+
+        this.forgotPasswordReinitialiseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                forgotPasswordMessageSentLabel.setText(getResources().getString(R.string.forgot_password_message_sent_label));
+                forgotPasswordMessageSentLabel.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+}
---
+++ Devoir03/app/src/main/java/com/example/devoir03/LoginActivity.java
@@ -0,0 +1,90 @@
+package com.example.devoir03;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.w3c.dom.Text;
+
+public class LoginActivity extends AppCompatActivity {
+    private Button loginButton;
+    private TextInputEditText emailTextBox;
+    private TextInputEditText passwordTextBox;
+    private TextView connectionErrorLabel;
+
+    private TextView forgotPasswordLabel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        this.loginButton = findViewById(R.id.loginButton);
+        this.emailTextBox = findViewById(R.id.emailTextBox);
+        this.passwordTextBox = findViewById(R.id.passwordTextBox);
+        this.connectionErrorLabel = findViewById(R.id.connectionErrorLabel);
+        this.connectionErrorLabel.setVisibility(View.GONE);
+        this.forgotPasswordLabel = findViewById(R.id.forgotPasswordLabel);
+
+        this.setForgotPasswordLinkClickable();
+
+        this.loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (auth()) {
+                    // TODO : Change to next activity, uncomment and change NEXT_ACTIVITY with the next activity name
+                    // Intent nextActivity = new Intent(LoginActivity.this, NEXT_ACTIVITY.class);
+                    // startActivity(nextActivity);
+                }
+                else {
+                    connectionErrorLabel.setText(getResources().getString(R.string.wrong_email_password_combinaison_label));
+                    connectionErrorLabel.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+    }
+
+    private boolean auth()
+    {
+        if (this.emailTextBox.getText().toString().equals("Test") && this.passwordTextBox.getText().toString().equals("1234")) {
+            return true;
+        }
+        return false;
+    }
+
+    private void setForgotPasswordLinkClickable()
+    {
+        SpannableString spannableString = new SpannableString(this.forgotPasswordLabel.getText());
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(View view) {
+                Intent nextActivity = new Intent(LoginActivity.this, ForgotPwdActivity.class);
+                startActivity(nextActivity);
+            }
+
+            @Override
+            public void updateDrawState(TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setColor(getColor(R.color.main_light_blue));
+
+            }
+        };
+        spannableString.setSpan(clickableSpan, 0, spannableString.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        this.forgotPasswordLabel.setText(spannableString);
+        this.forgotPasswordLabel.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+}
