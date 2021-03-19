12603b9459452652a68a03a3424a8cb495311f8d
LauraSenegas <laurasenegas@hotmail.com>
2021-03-18 22:10:51 -0400

    Amélioration de la page paramètres et ajouts de commentaire

--- Devoir3CoursAntoine/app/src/main/java/com/example/devoir3/Parametres.java
+++ Devoir3CoursAntoine/app/src/main/java/com/example/devoir3/Parametres.java
@@ -1,18 +1,12 @@
 package com.example.devoir3;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.preference.PreferenceFragmentCompat;
-import android.widget.TextView;
-
-
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Parametres extends AppCompatActivity {
-    TextView tvParamètres;
-    ImageView ivMonProfilParametres;
-
+    BottomNavigationView bottomNavigationMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +14,36 @@ public class Parametres extends AppCompatActivity {
         setContentView(R.layout.activity_parametres);
     }
 
-    //Lorsque l'utilisateur clique sur l'icône "<", il est ramené vers la page précédente
+    // Lorsque l'utilisateur clique sur l'icône "<", il est ramené vers la page précédente
     public void quitterParam(View v){ this.finish(); }
+
+    // Méthode pour initialiser le fonctionnement de la barre de navigation
+    public void setOnclickNavBar(){
+        bottomNavigationMenu=findViewById(R.id.barnavigationNotifsInfo);
+        bottomNavigationMenu.setOnNavigationItemSelectedListener(item -> {
+            if(item.getItemId()==R.id.mescours){
+                Intent intent=new Intent(Parametres.this, MesCours.class);
+                startActivity(intent);
+                return true;
+            }
+            else if(item.getItemId()==R.id.search){
+                Intent intent=new Intent(Parametres.this,RechercherCours.class);
+                startActivity(intent);
+                return true;
+            }
+            else if(item.getItemId()==R.id.calendrier){
+                Intent intent=new Intent(Parametres.this,CalendarActivity.class);
+                startActivity(intent);
+                return true;
+            }
+            else if(item.getItemId()==R.id.message){
+                Intent intent=new Intent(Parametres.this,Messagerie.class);
+                startActivity(intent);
+                return true;
+            }
+            return false;
+        });
+    }
 }
 
 
